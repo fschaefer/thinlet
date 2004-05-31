9950807e6bc051ff4aa10d9387905e8a3529ca01
abial <>
2004-05-31 20:09:57 +0000

    Add ability to set your own icon in the constructor to FrameLauncher.
    Submitted by Benjamin Brown.

--- thinlet/src/java/thinlet/FrameLauncher.java
+++ thinlet/src/java/thinlet/FrameLauncher.java
@@ -27,6 +27,24 @@ public class FrameLauncher extends Frame implements WindowListener {
 	 * @param height the preferred height of the content
 	 */
 	public FrameLauncher(String title, Thinlet content, int width, int height) {
+		this(title, null, content, width, height, true) ;
+	}
+	
+	/**
+	 * Construct a new frame with the specified title, including the
+	 * given <i>thinlet</i> component. The frame is centered on the screen, and its
+	 * preferred size is specified (excluding the frame's borders). The frame icon is
+	 * the parameter icon unless it is null, or has not completed loading in which 
+	 * case the default thinlet one will be used instead
+	 * 
+	 * @param title the title to be displayed in the frame's border
+	 * @param icon the image to be displayed in the frame's border
+	 * @param content a <i>thinlet</i> instance
+	 * @param width the preferred width of the content
+	 * @param height the preferred height of the content
+	 * @param visible whether to make the frame visible immediately
+	 */
+	public FrameLauncher(String title, Image icon, Thinlet content, int width, int height, boolean visible) {
 		super(title);
 		this.content = content;
 		add(content, BorderLayout.CENTER);
@@ -40,22 +58,25 @@ public class FrameLauncher extends Frame implements WindowListener {
 		width = Math.min(width, ss.width);
 		height = Math.min(height, ss.height);
 		setBounds((ss.width - width) / 2, (ss.height - height) / 2, width, height); 
-		setVisible(true);
 		//maximize: setBounds(-is.left, -is.top, ss.width + is.left + is.right, ss.height + is.top + is.bottom);
 		
-		int[] pix = new int[16 * 16];
-		for (int x = 0; x < 16; x++) {
-			int sx = ((x >= 1) && (x <= 9)) ? 1 : (((x >= 11) && (x <= 14)) ? 2 : 0);
-			for (int y = 0; y < 16; y++) {
-				int sy = ((y >= 1) && (y <= 9)) ? 1 : (((y >= 11) && (y <= 14)) ? 2 : 0);
-				pix[y * 16 + x] = ((sx == 0) || (sy == 0)) ? 0xffffffff :
-					((sx == 1) ? ((sy == 1) ? (((y == 2) && (x >= 2) && (x <= 8)) ? 0xffffffff :
-						(((y >= 3) && (y <= 8)) ? ((x == 5) ? 0xffffffff : (((x == 4) || (x == 6)) ?
-							0xffe8bcbd : 0xffb01416)) : 0xffb01416)) : 0xff377ca4) :
-						((sy == 1) ? 0xff3a831d : 0xfff2cc9c)); 
+		if (icon == null || icon.getWidth(null) <=0 || icon.getHeight(null) <= 0) {
+			int[] pix = new int[16 * 16];
+			for (int x = 0; x < 16; x++) {
+				int sx = ((x >= 1) && (x <= 9)) ? 1 : (((x >= 11) && (x <= 14)) ? 2 : 0);
+				for (int y = 0; y < 16; y++) {
+					int sy = ((y >= 1) && (y <= 9)) ? 1 : (((y >= 11) && (y <= 14)) ? 2 : 0);
+					pix[y * 16 + x] = ((sx == 0) || (sy == 0)) ? 0xffffffff :
+						((sx == 1) ? ((sy == 1) ? (((y == 2) && (x >= 2) && (x <= 8)) ? 0xffffffff :
+							(((y >= 3) && (y <= 8)) ? ((x == 5) ? 0xffffffff : (((x == 4) || (x == 6)) ?
+								0xffe8bcbd : 0xffb01416)) : 0xffb01416)) : 0xff377ca4) :
+							((sy == 1) ? 0xff3a831d : 0xfff2cc9c)); 
+				}
 			}
+			icon = createImage(new MemoryImageSource(16, 16, pix, 0, 16)) ;
 		}
-		setIconImage(createImage(new MemoryImageSource(16, 16, pix, 0, 16)));
+		setIconImage(icon);
+		setVisible(visible);
 	}
 		
 	/**
